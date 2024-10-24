26872834c3be314dc5a5f7ff93bcdc9da8c9a05d
AndreasDTU <s235455@dtu.dk>
2024-10-24 14:15:26 +0100

    Added test

---
+++ Tests/Unit tests/unit.test.js
@@ -0,0 +1,32 @@
+import '@testing-library/jest-dom/extend-expect';
+import { JSDOM } from 'jsdom';
+import fs from 'fs';
+import path from 'path';
+
+describe('CV Generator Website', () => {
+    let dom;
+
+    beforeAll(() => {
+        // Load the HTML file
+        const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+        dom = new JSDOM(html);
+    });
+
+    test('should load the HTML content', () => {
+        const document = dom.window.document;
+        expect(document.title).toBe('CV Generator');
+        expect(document.querySelector('h1')).toHaveTextContent('CV Generator');
+        expect(document.querySelector('form')).toBeInTheDocument();
+    });
+
+    test('should load the CSS file', () => {
+        const link = dom.window.document.querySelector('link[rel="stylesheet"]');
+        expect(link).toBeInTheDocument();
+        expect(link.href).toContain('style.css');
+    });
+
+    test('should load the JavaScript file', () => {
+        const script = dom.window.document.querySelector('script[src="script.js"]');
+        expect(script).toBeInTheDocument();
+    });
+});
